6bbd481ee56844492162388e27a106b12a7e71d9
Niko3727 <niko__@live.fr>
2024-04-19 10:24:55 +0200

    Creation of the register and login page

---
+++ src/Controller/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\UserManager;
+
+class UserController extends AbstractController
+{
+    public function login()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $credentials = array_map('trim', $_POST);
+            $userManager = new UserManager();
+            $user = $userManager->selectOneByEmail($credentials['email']);
+            if ($user && password_verify($credentials['password'], $user['password'])) {
+                $_SESSION['user_id'] = $user['id'];
+                header('location: /');
+                exit();
+            }
+        }
+    }
+
+    public function register()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $credentials = $_POST;
+            $userManager = new UserManager();
+            if ($userManager->insert($credentials)) {
+                return $this->login();
+            }
+        }
+        return $this->twig->render('User/register.html.twig');
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -14,4 +14,6 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
+    'register' => ['UserController', 'register',],
+    'login' => ['UserController', 'login',],
 ];
